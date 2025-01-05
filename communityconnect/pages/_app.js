60e4a4656c9a4395a3c803a83d0306180eb795ee
spanja05 <simone.panja@gmail.com>
2025-01-05 16:17:41 -0500

    add app, header, and homepage components

--- communityconnect/pages/_app.js
+++ communityconnect/pages/_app.js
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "../public/css/styles.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- communityconnect/pages/_document.js
+++ communityconnect/pages/_document.js
@@ -3,9 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
-        <Main />
+        <div className="container">
+          <Main />
+        </div>
         <NextScript />
       </body>
     </Html>
